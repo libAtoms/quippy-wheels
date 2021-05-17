f637150e4287885b81678fb6f4e2302d8606ccef
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-05-17 19:21:03 +0100

    WIP

--- config.sh
+++ config.sh
@@ -1,13 +1,44 @@
 # Define custom utilities
 # Test for macOS with [ -n "$IS_MACOS" ]
 
+source gfortran-install/gfortran_utils.sh
+
+function _build_wheel {
+    build_libs
+    build_bdist_wheel $@
+}
+
+function build_wheel {
+    if [ -n "$IS_MACOS" ]; then
+        install_gfortran
+    fi
+    echo gcc --version
+    echo `gcc --version`
+    # Fix version error for development wheels by using bdist_wheel
+    wrap_wheel_builder _build_wheel $@
+}
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
-    # Runs in the root directory of this repository.
+    # Runs in the root directory of this repository
+
+    # setuptools v49.2.0 is broken
+    $PYTHON_EXE -mpip install --upgrade "setuptools<49.2.0"
+    # Use the same incantation as numpy/tools/travis-before-install.sh to
+    # download and un-tar the openblas libraries. The python call returns
+    # the un-tar root directory, then the files are copied into /usr/local.
+    # Could utilize a site.cfg instead to prevent the copy.
+    $PYTHON_EXE -mpip install urllib3
+    $PYTHON_EXE -c"import platform; print('platform.uname().machine', platform.uname().machine)"
+    basedir=$($PYTHON_EXE numpy/tools/openblas_support.py)
+    $use_sudo cp -r $basedir/lib/* $BUILD_PREFIX/lib
+    $use_sudo cp $basedir/include/* $BUILD_PREFIX/include
+    export OPENBLAS=$BUILD_PREFIX
+
     export QUIP_ARCH=linux_x86_64_gfortran
     cd ${REPO_DIR}
     mkdir -p build/${QUIP_ARCH}
-    cp travis/Makefile.inc build/${QUIP_ARCH}/Makefile.inc
+    cp Makefile.inc build/${QUIP_ARCH}/Makefile.inc
     make quippy
 }
 
