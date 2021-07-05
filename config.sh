99b6e7cea830ca5a527351840054837d10e5e54b
James Kermode <james.kermode@gmail.com>
2021-07-05 20:22:48 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -19,16 +19,29 @@ source gfortran-install/gfortran_utils.sh
 #     echo " --use-feature=in-tree-build"
 # }
 
+function install_delocate {
+    check_pip
+    $PIP_CMD install git+https://github.com/isuruf/delocate.git@arm64
+}
+
+
 function pre_build {    
     install_gfortran
     
     # fetch and install OpenBLAS in same way as its done for numpy
-#     build_openblas
-
-    mkdir -p archives
-    local plat=${1:-${PLAT:-x86_64}}
-    local tar_path=$(abspath $(openblas_get $plat))
-    (cd / && tar zxf $tar_path)
+    # setuptools v49.2.0 is broken
+    $PYTHON_EXE -mpip install --upgrade "setuptools<49.2.0"
+    # Use the same incantation as numpy/tools/travis-before-install.sh to
+    # download and un-tar the openblas libraries. The python call returns
+    # the un-tar root directory, then the files are copied into /usr/local.
+    # Could utilize a site.cfg instead to prevent the copy.
+    $PYTHON_EXE -mpip install urllib3
+    $PYTHON_EXE -c"import platform; print('platform.uname().machine', platform.uname().machine)"
+    curl https://raw.githubusercontent.com/numpy/numpy/main/tools/openblas_support.py -o openblas_support.py
+    basedir=$($PYTHON_EXE openblas_support.py)
+    $use_sudo cp -r $basedir/lib/* $BUILD_PREFIX/lib
+    $use_sudo cp $basedir/include/* $BUILD_PREFIX/include
+    export OPENBLAS=$BUILD_PREFIX
 
     # install build dependencies (i.e. oldest supported numpy) before f90wrap,
     # otherwise we get too new a version
